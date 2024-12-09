b9585c529139a0521d15858212509b6a4bf24b48
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-12-09 13:22:11 +0100

    HeaderComponent and Styled

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,16 @@
-import style from './header.module.scss'
-
-export const Header = () =>{
-    return(
-        <header className={style.headerStyle}>
-            <h1>Mikkel Mussen</h1>
-        </header>
+import headerBG from '../../assets/HeaderBG.png';
+import { HeaderStyled } from './Header.styled';
+export const Header = () => {
+    return (
+        <HeaderStyled>
+            
+                {/* <img src={headerBG} alt="Header Image" /> */}
+                <h1>Under the Moon</h1>
+                <div>
+                    <h2>HAIRSTYLIST OG FRISØR</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla nostrum similique soluta earum sunt, sequi non laborum quam tempora vel aperiam eveniet consequatur, maxime error neque harum inventore natus qui!</p>
+                </div>
+            
+        </HeaderStyled>
     )
 };
