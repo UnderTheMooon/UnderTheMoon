14faafa3ad65186afe9dea9a1b5f0d01ceeedf0f
MikkelTMS <mtamsgaard@gmail.com>
2024-12-13 12:56:16 +0100

    update
    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import './App.scss'
 import { Nav } from './components/nav/nav.jsx'
+<<<<<<< Updated upstream
 import { Header  } from './components/header/header.jsx'
 import { ReadMoreSection } from './components/ReadMoreSection/ReadMoreSection.component.jsx'
 import { BookTimeSection } from './components/BookTimeSection/BookTimeSection.component.jsx'
 import { InstagramSection } from './components/InstagramSection/InstagramSection.component.jsx'
 
+=======
+import { Header } from './components/header/header.jsx'
+>>>>>>> Stashed changes
 function App() {
 
   return (
     <>
     <Nav/>
     <Header/>
+<<<<<<< Updated upstream
     <ReadMoreSection/>
     <BookTimeSection/>
     <InstagramSection/>
+=======
+>>>>>>> Stashed changes
     </>
   )
 }
